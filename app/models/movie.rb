91e08fa38fc95e0849d205f9333aca8a5e2c27fb
viabledish <viable.dish@gmail.com>
2013-11-19 20:30:27 +0000

    using some helper methods

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -22,6 +22,11 @@ class Movie < ActiveRecord::Base
 
   validate :release_date_is_in_the_future
 
+  def review_average
+    return 0 if reviews.empty?
+    reviews.sum(:rating_out_of_ten)/reviews.size
+  end
+
   protected
 
   def release_date_is_in_the_future
