466a64e079328b16b944485294fb4053a44f38c4
viabledish <viable.dish@gmail.com>
2013-11-19 05:11:39 +0000

    displaying flash notices and alerts

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+  	user = User.find_by(email: params[:email])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to movies_path, notice: "Welcome back, #{user.firstname}!"
+    else
+    	flash.now[:alert] = "Log in failed..."
+      render :new
+    end
   end
 end
